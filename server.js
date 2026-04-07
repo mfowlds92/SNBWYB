928c18f5dbff11fab45dd007d8cc1412ac1a532c
mfowlds92 <65459199+mfowlds92@users.noreply.github.com>
2026-04-07 22:22:27 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1009,8 +1009,6 @@ socket.on("chooseKnockOrGuru", ({ roomId, choice }) => {
 
 
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(3000, () =>
+  console.log("Server running on http://localhost:3000")
+);
